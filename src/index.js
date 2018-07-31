ba974c6175d49071072b21d4b91f44ee1b33c276
Paia Thao <paiathao09@gmail.com>
2018-07-31 14:49:42 -0500

    store and Provider set up

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const storeInstance = createStore ( 
+    () => { 
+        console.log('Hey I’m a reducer') 
+        
+    }, 
+   
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
